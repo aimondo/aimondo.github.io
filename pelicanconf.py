b8a606e236956c1e113af0ae827c49381b0da50b
aallouache3 <aimon.allouache@gmail.com>
2019-04-14 10:19:13 -0500

    updating with latest info

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,8 +3,9 @@
 from __future__ import unicode_literals
 
 AUTHOR = 'Team89'
-SITENAME = 'HeadToHead'
+SITENAME = 'SeaminglyAccurate'
 SITEURL = 'http://aimondo.github.io'
+FAVICON = "/images/favicon.png"
 
 PATH = 'content'
 
@@ -39,4 +40,23 @@ MARKUP = ('md', 'ipynb')
 PLUGIN_PATHS = './plugins'
 PLUGINS = ['ipynb.markup']
 
-THEME = '/Users/aimonallouache/Documents/Georgia_Tech/Spring_2019/CS6242/GroupProject/pelican-themes/attila'
+THEME = '../pelican-themes/attila'
+
+AUTHORS_BIO = {
+  "Team89": {
+    "name": "Team89",
+    "cover": "https://upload.wikimedia.org/wikipedia/en/1/1e/Baseball_%28crop%29.jpg",
+    "image": "https://upload.wikimedia.org/wikipedia/en/1/1e/Baseball_%28crop%29.jpg",
+    "website": "http://aimondo.github.io",
+    "location": "OMSCS",
+    "bio": "We are a group of CSE6242 students who are trying to Beat the Streak"
+  }
+}
+
+HOME_COVER = '/assets/images/baseball-math.png'
+
+STATIC_PATHS = ['images', 'extra/robots.txt', 'extra/favicon.ico']
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'}
+}
